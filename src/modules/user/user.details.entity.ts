e7c547c18cd085cd4e0a5cb0f2026cfd2dec3e30
JuanFernandezJubin <fernandezjubin.data@gmail.com>
2021-04-19 17:26:18 -0300

    fixing user entity. Roles and UserDetails done.

---
+++ src/modules/user/user.details.entity.ts
@@ -0,0 +1,23 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users_details')
+export class UserDetails extends BaseEntity {
+
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column({ type: 'varchar', unique: true, length: 50, nullable: false })
+    name: string;
+
+    @Column({ type: 'varchar', nullable: false })
+    lastname: string;
+
+    @Column({ type: 'varchar', default: 'ACTIVE', length: 8 })
+    status: string;
+
+    @Column({ type: 'timestamp', name: 'created_at' })
+    createdAt: Date;
+
+    @Column({ type: 'timestamp', name: 'updated_at' })
+    uptdatedAt: Date;
+}
